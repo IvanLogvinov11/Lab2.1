3ea4658e83f51585d3bbb353767acd9e847bd347
IvanLogvinov11 <logvinov123vanya@mail.ru>
2022-12-09 23:53:47 +0300

    new1

---
+++ Новая папка/numbers.py
@@ -0,0 +1,11 @@
+print("enter four numb: ")
+numb1 = int(input())
+numb2 = int(input())
+numb3 = int(input())
+numb4 = int(input())
+
+sum1 = numb1 + numb2
+sum2 = numb3 + numb4
+div = sum1 / sum2
+
+print("we have made some calculation\nthe result is: ",format(div,".3"))
